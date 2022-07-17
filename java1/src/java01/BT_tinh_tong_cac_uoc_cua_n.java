4fadfb63ebce67ffc6e003ff221e4e9674bb93bd
vanquyet <quyet2k2ocsa@gmail.com>
2022-07-17 18:21:11 +0700

    cap nhat

--- java1/src/java01/BT_tinh_tong_cac_uoc_cua_n.java
+++ java1/src/java01/BT_tinh_tong_cac_uoc_cua_n.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class BT_tinh_tong_cac_uoc_cua_n {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap n: ");
 		int n = sc.nextInt();
 		int sum = 0;
 		for(int i =1;i<=n;i++)
