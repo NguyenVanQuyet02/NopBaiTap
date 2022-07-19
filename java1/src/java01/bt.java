a373ae4d325f76cbb67e28b2607f31135e5fce63
vanquyet <quyet2k2ocsa@gmail.com>
2022-07-19 17:30:13 +0700

    update

--- java1/src/java01/bt.java
+++ java1/src/java01/bt.java
@@ -9,29 +9,29 @@ public class bt {
 		m = scanner.nextInt();
 		if(d > 0 && d <=31 && m > 0 && m <=12)
 		{
-			if(d >= 20 && m==1 || d <=18 && m ==2)
+			if(d >= 20 && m==1 && d <=31 || d <=18 && m ==2)
 				System.out.println("Cung hoang dao cua bn la Bao Binh");
 			else if(d >= 19 && d <=28 && m==2 || d <=20 && m ==3)
 				System.out.println("Cung hoang dao cua bn la Song Ngu");
-			else if(d >= 21 && m==3 || d <=20 && m ==4)
+			else if(d >= 21 && m==3 && d <=31 || d <=20 && m ==4)
 				System.out.println("Cung hoang dao cua bn la Bach Duong");
-			else if(d >= 21 && m==4 || d <=20 && m ==5)
+			else if(d >= 21 && m==4 && d <=30|| d <=20 && m ==5)
 				System.out.println("Cung hoang dao cua bn la Kim Nguu");
-			else if(d >= 21 && m==5 || d <=21 && m ==6)
+			else if(d >= 21 && m==5 && d <=31|| d <=21 && m ==6)
 				System.out.println("Cung hoang dao cua bn la Song Tu");
-			else if(d >= 22 && m==6 || d <=22 && m ==7)
+			else if(d >= 22 && m==6 && d <=30 || d <=22 && m ==7)
 				System.out.println("Cung hoang dao cua bn la Cu Giai");
-			else if(d >= 23 && m==7 || d <=22 && m ==8)
+			else if(d >= 23 && m==7 && d <=31|| d <=22 && m ==8)
 				System.out.println("Cung hoang dao cua bn la Su Tu");
-			else if(d >= 23 && m==8 || d <=22 && m ==9)
+			else if(d >= 23 && m==8 && d <=31|| d <=22 && m ==9)
 				System.out.println("Cung hoang dao cua bn la Xu Nu");
-			else if(d >= 23 && m==9 || d <=23 && m ==10)
+			else if(d >= 23 && m==9 && d <=30|| d <=23 && m ==10)
 				System.out.println("Cung hoang dao cua bn la Thien Binh");
-			else if(d >= 24 && m==10 || d <=22 && m ==11)
+			else if(d >= 24 && m==10 && d <=31|| d <=22 && m ==11)
 				System.out.println("Cung hoang dao cua bn la Bo Cap");
-			else if(d >= 23 && m==11 || d <=21 && m ==12)
+			else if(d >= 23 && m==11 && d <=30|| d <=21 && m ==12)
 				System.out.println("Cung hoang dao cua bn la Nhan Ma");
-			else if(d >= 22 && m==12 || d <=19 && m ==1)
+			else if(d >= 22 && m==12 && d <=31|| d <=19 && m ==1)
 				System.out.println("Cung hoang dao cua bn la Ma Ket");
 			else
 				System.out.println("Ngay Thang ban nhap sai");
